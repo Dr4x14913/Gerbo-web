aebe8a1d4fb803b4505f004333693684406d558f
Claire Antoine <claire.antoine56@gmail.com>
2024-02-12 22:55:28 +0100

    update: app.py

--- app/app.py
+++ app/app.py
@@ -11,33 +11,32 @@ import dash_bootstrap_components as dbc
 #-- SQL init
 #--------------------------------------------------------------------------------------------------------
 
-if False:
-    MYSQL_USER     = 'user'
-    MYSQL_PASSWORD = 'password'
-    MYSQL_DATABASE = 'website'
+MYSQL_USER     = 'user'
+MYSQL_PASSWORD = 'password'
+MYSQL_DATABASE = 'website'
 
-    #Establish the connection to MariaDB
-    db = Sql(MYSQL_DATABASE, DB_HOST='db', DB_USER=MYSQL_USER, DB_PASS=MYSQL_PASSWORD)
-    print("Connection successful!")
+#Establish the connection to MariaDB
+db = Sql(MYSQL_DATABASE, DB_HOST='db', DB_USER=MYSQL_USER, DB_PASS=MYSQL_PASSWORD)
+print("Connection successful!")
 
-    # Create the "users" table
-    db.insert("""
-    CREATE TABLE IF NOT EXISTS `users` (
-        `id` INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        `username` VARCHAR(30) NOT NULL,
-        `password` VARCHAR(100) NOT NULL,
-        `email` VARCHAR(50),
-        `registration_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-        )""")
-    # Insert an admin user with username "admin", password set at "123" and email "admin@example.com"
-    db.insert("""
-        INSERT INTO users (username, password, email)
-        SELECT * FROM (SELECT 'admin' as username, '123' as password, 'admin@example.com' as email) AS tmp
-        WHERE NOT EXISTS (
-            SELECT username FROM users WHERE username = 'admin'
-        ) LIMIT 1
-    """)
-    print('Admin user created!')
+# Create the "users" table
+db.insert("""
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(30) NOT NULL,
+    `password` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(50),
+    `registration_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )""")
+# Insert an admin user with username "admin", password set at "123" and email "admin@example.com"
+db.insert("""
+    INSERT INTO users (username, password, email)
+    SELECT * FROM (SELECT 'admin' as username, '123' as password, 'admin@example.com' as email) AS tmp
+    WHERE NOT EXISTS (
+        SELECT username FROM users WHERE username = 'admin'
+    ) LIMIT 1
+""")
+print('Admin user created!')
 
 #--------------------------------------------------------------------------------------------------------
 #-- APP init
