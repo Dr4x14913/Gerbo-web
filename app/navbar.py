ad5d68225a9dbb11c926734e9c33ccda9a44f48d
Nathan Daviaud <nathan.daviaud@allegrodvt.com>
2024-02-12 21:17:58 +0100

    feat: add assets folder and logo

--- app/navbar.py
+++ app/navbar.py
@@ -1,10 +1,15 @@
 import dash_bootstrap_components as dbc
+from dash import html
 
 def get_navbar(pages):
+    logo = html.Img(src='assets/gerbouillette.jpg', height="100px"),
+
     rows = [dbc.Col(dbc.NavLink(page['name'], href=page['relative_path'])) for page in pages]
     navbar = dbc.Navbar(
     dbc.Container([
-        dbc.Row([dbc.Col("Logo")]),
+        # Place this logo to the left
+        dbc.Row([dbc.Col(logo)]),
         dbc.Row(rows)
     ]))
+
     return navbar
