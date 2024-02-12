7dff64e2605c8ad02bb1eb25d9bd6e547e9092d9
Claire Antoine <claire.antoine56@gmail.com>
2024-02-12 22:52:10 +0100

    chore: start of front design with css

--- app/app.py
+++ app/app.py
@@ -10,42 +10,45 @@ import dash_bootstrap_components as dbc
 #--------------------------------------------------------------------------------------------------------
 #-- SQL init
 #--------------------------------------------------------------------------------------------------------
-MYSQL_USER     = 'user'
-MYSQL_PASSWORD = 'password'
-MYSQL_DATABASE = 'website'
 
-#Establish the connection to MariaDB
-db = Sql(MYSQL_DATABASE, DB_HOST='db', DB_USER=MYSQL_USER, DB_PASS=MYSQL_PASSWORD)
-print("Connection successful!")
+if False:
+    MYSQL_USER     = 'user'
+    MYSQL_PASSWORD = 'password'
+    MYSQL_DATABASE = 'website'
 
-# Create the "users" table
-db.insert("""
-CREATE TABLE IF NOT EXISTS `users` (
-    `id` INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    `username` VARCHAR(30) NOT NULL,
-    `password` VARCHAR(100) NOT NULL,
-    `email` VARCHAR(50),
-    `registration_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    )""")
-# Insert an admin user with username "admin", password set at "123" and email "admin@example.com"
-db.insert("""
-    INSERT INTO users (username, password, email)
-    SELECT * FROM (SELECT 'admin' as username, '123' as password, 'admin@example.com' as email) AS tmp
-    WHERE NOT EXISTS (
-        SELECT username FROM users WHERE username = 'admin'
-    ) LIMIT 1
-""")
-print('Admin user created!')
+    #Establish the connection to MariaDB
+    db = Sql(MYSQL_DATABASE, DB_HOST='db', DB_USER=MYSQL_USER, DB_PASS=MYSQL_PASSWORD)
+    print("Connection successful!")
+
+    # Create the "users" table
+    db.insert("""
+    CREATE TABLE IF NOT EXISTS `users` (
+        `id` INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        `username` VARCHAR(30) NOT NULL,
+        `password` VARCHAR(100) NOT NULL,
+        `email` VARCHAR(50),
+        `registration_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        )""")
+    # Insert an admin user with username "admin", password set at "123" and email "admin@example.com"
+    db.insert("""
+        INSERT INTO users (username, password, email)
+        SELECT * FROM (SELECT 'admin' as username, '123' as password, 'admin@example.com' as email) AS tmp
+        WHERE NOT EXISTS (
+            SELECT username FROM users WHERE username = 'admin'
+        ) LIMIT 1
+    """)
+    print('Admin user created!')
 
 #--------------------------------------------------------------------------------------------------------
 #-- APP init
 #--------------------------------------------------------------------------------------------------------
 app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = html.Div([
-    get_navbar(dash.page_registry.values()),
+    html.Div(children = [html.Img(src='assets/logo.png', id='logo'),
+                        get_navbar(dash.page_registry.values())], id='header'),
     dash.page_container
     # content will be here
-])
+], id='app-container')
 
 #--------------------------------------------------------------------------------------------------------
 #-- MAIN
--- app/navbar.py
+++ app/navbar.py
@@ -1,15 +1,8 @@
 import dash_bootstrap_components as dbc
 from dash import html
 
-def get_navbar(pages):
-    logo = html.Img(src='assets/gerbouillette.jpg', height="100px"),
-
-    rows = [dbc.Col(dbc.NavLink(page['name'], href=page['relative_path'])) for page in pages]
-    navbar = dbc.Navbar(
-    dbc.Container([
-        # Place this logo to the left
-        dbc.Row([dbc.Col(logo)]),
-        dbc.Row(rows)
-    ]))
-
+def get_navbar(pages)->html.Div:
+    """TODO"""
+    rows = [(dbc.NavLink(page['name'], href=page['relative_path'], class_name='navlink')) for page in pages]
+    navbar = html.Div(children = rows, id='navbar')
     return navbar
