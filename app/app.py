8c8d4579385bf519dac8ca5be3816bf11255b7f9
Nathan Daviaud <nathan.daviaud@allegrodvt.com>
2024-02-15 16:12:59 +0100

    feat: add team and displat_name column to sql table

--- app/app.py
+++ app/app.py
@@ -21,13 +21,14 @@ CREATE TABLE IF NOT EXISTS `users` (
     `id` INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     `username` VARCHAR(30) NOT NULL UNIQUE,
     `password` VARCHAR(100) NOT NULL,
-    `email` VARCHAR(50),
+    `display_name` VARCHAR(50),
+    `team` VARCHAR(50),
     `registration_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     )""")
 # Insert an admin user with username "admin", password set at "123" and email "admin@example.com"
 db.insert("""
-    INSERT INTO users (username, password, email)
-    SELECT * FROM (SELECT 'admin' as username, '123' as password, 'admin@example.com' as email) AS tmp
+    INSERT INTO users (username, password)
+    SELECT * FROM (SELECT 'admin' as username, '123' as password) AS tmp
     WHERE NOT EXISTS (
         SELECT username FROM users WHERE username = 'admin'
     ) LIMIT 1
