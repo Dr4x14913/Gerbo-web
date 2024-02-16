9d9bc9bba11f4d66e6abe4b1b409b7aa388af4c6
Nathan Daviaud <nathan.daviaud@allegrodvt.com>
2024-02-16 16:53:52 +0100

    feat: upgrade basic back office

--- app/backoffice_manager.py
+++ app/backoffice_manager.py
@@ -1,4 +1,22 @@
 #! /usr/bin/python3
 from sql import *
+import pandas as pd
 
-db = Sql(MYSQL_DATABASE, DB_HOST='db', DB_USER=MYSQL_USER, DB_PASS=MYSQL_PASSWORD)
+
+def get_db_data_as_df():
+    db = Sql(MYSQL_DATABASE, DB_HOST='db', DB_USER=MYSQL_USER, DB_PASS=MYSQL_PASSWORD)
+    df = db.select_to_df("Select username, display_name, team, avatar_name from users where username != 'admin'", ("username", "display_name", "team", "avatar_name"))
+    db.close()
+    return df
+
+def update_table(update_dict):
+    """update_dict should be contruct as follow: {username: {col1: val1, col2: val2 ...}, username2: {...}}"""
+    try:
+        db = Sql(MYSQL_DATABASE, DB_HOST='db', DB_USER=MYSQL_USER, DB_PASS=MYSQL_PASSWORD)
+        for user in update_dict:
+            sql_req = "UPDATE users SET " + ", ".join([f"{c}='{v}'" for c,v in update_dict[user].items()]) + f" WHERE username='{user}'"
+            db.insert(sql_req)
+        db.close()
+    except Exception as e:
+        return f"Error: {e}"
+    return ""
--- app/pages/backoffice.py
+++ app/pages/backoffice.py
@@ -1,8 +1,12 @@
 import dash
-from dash import html, dcc, callback, Input, Output, State
+from dash.exceptions import PreventUpdate
+from dash import html, dcc, callback, Input, Output, State, ALL
 import dash_bootstrap_components as dbc
 import pandas as pd
+from backoffice_manager import get_db_data_as_df, update_table
 
+#------------------------------------------------------------------------------------
+dash.register_page(__name__)
 
 content =  dbc.Card(
     [
@@ -19,16 +23,14 @@ content =  dbc.Card(
         dbc.CardBody(html.P(id="card-content", className="card-text")),
     ]
 )
-
-
-#------------------------------------------------------------------------------------
-dash.register_page(__name__)
-
 layout = html.Div([
-    html.Div([], id='backoffice-content'),
+    html.Div([content], id='backoffice-content'),
     html.Br(),
+    html.Div(["jj:"], id="output"),
 ])
 
+#------------------------------------------------------------------------------------
+#-- Callbacks
 #------------------------------------------------------------------------------------
 @callback(
     Output("backoffice-content", "children"),
@@ -40,3 +42,53 @@ def access(dummy, user):
         return content
     else:
         return dbc.Alert('Permission denied', color="danger")
+
+@callback(
+    Output("card-content", "children"),
+    [Input("card-tabs", "active_tab")],
+)
+def tab_content_callback(tab):
+    if tab == "tab-1":
+        table = dbc.Table.from_dataframe(get_db_data_as_df(), striped=True, bordered=True, hover=True)
+        content = table
+    else:
+        # Display a dash_bootstrap_components table where each field is an input text
+        data         = get_db_data_as_df().to_dict('split')
+        username_idx = data['columns'].index("username")
+        idx          = {data['columns'].index(colname): colname for colname in data['columns'] }
+        table_header = [html.Thead([html.Tr([html.Th(h) for h in data['columns']])])]
+        table_rows   = [
+            html.Tbody(
+                       [html.Tr([html.Td(get_input(elem,f"{row[username_idx]}-{idx[row.index(elem)]}") if row.index(elem) != username_idx else elem) for elem in row]) for row in data['data']]
+            )
+        ]
+        table = dbc.Table(
+            table_header + table_rows,
+            bordered=True,
+            striped=True,
+        )
+        content = html.Div([table, dbc.Button("Send", color="success", className="me-1", id="send-btn")])
+    return content
+# dash callback that fetch the value of all input text field with their id beginning with "input-"
+
+@callback(Output('output', 'children'),
+          [Input("send-btn", 'n_clicks')],
+          [State({"type":"in-db", "index": ALL}, "value"), State({"type":"in-db", "index": ALL}, "id")]
+    )
+def display_output(clicks, values, ids):
+    if clicks is None:
+        raise PreventUpdate
+    updates = {}
+    for val, idx in zip(values, [i['index'] for i in ids]):
+        username, colname = idx.split("-")
+        if username not in updates: updates[username] = {}
+        updates[username][colname] = val
+    res = update_table(updates)
+    return res
+
+#------------------------------------------------------------------------------------
+#-- Other functions
+#------------------------------------------------------------------------------------
+def get_input(text, id):
+    full_id = {"type":"in-db", "index":id}
+    return dbc.Input(value=text, type="text", id=full_id),
