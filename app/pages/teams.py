bc60d7165ca980947faf758e9d4174d018f0d08f
Arthur Moulinier <moulinierarthur@gmail.com>
2024-02-17 15:25:13 +0100

    fix: imports in teams.py

--- app/pages/teams.py
+++ app/pages/teams.py
@@ -1,7 +1,7 @@
 import dash
 from dash import html, dcc, callback, Input, Output, State
 import dash_bootstrap_components as dbc
-from teams_manager import get_team_color, get_all_teams, random_str
+from teams_manager import get_team_color, get_all_teams
 
 dash.register_page(__name__)
 
